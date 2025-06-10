5e17fe8e7c0e0626e1f0407798e262311ae758a4
lanxing520 <1461626683@qq.com>
2025-06-10 14:52:06 +0800

    add

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router"
-
 const HomePage = () => import("@/views/homepage/index.vue")
 const ExperimentPage = () => import("@/views/scene/experiment/index.vue")
-
-const rootRoute = import.meta.env.DEV
-  ? {
-      path: "/",
-      name: "homepage",
-      component: HomePage,
-    }
-  : {
-      path: "/",
-      redirect: "/experiment-page",
-    }
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    rootRoute,
+    {
+      path: "/",
+      name: "homepage",
+      component: HomePage,
+    },
     {
       path: "/experiment-page",
       name: "experiment-page",
--- src/views/homepage/index.vue
+++ src/views/homepage/index.vue
@@ -1,40 +1,28 @@
 <template>
   <div class="homepage">
-    <div class="tab" v-for="(item, i) in routes" :key="i" @click="goTo(item)">
-      {{ item.name }}
-    </div>
-    <div class="file-button" @click="dialogVisible = true">获取excel</div>
-    <el-dialog v-model="dialogVisible" title="读取文件" align-center width="90%">
-      <ReadExcel class="excel" @file-upload="getFileData" />
-      <template #footer>
-        <el-button @click="onConfirm">确定</el-button>
-      </template>
-    </el-dialog>
+    <img src="@/assets/img/homepage/enter_button.png" class="tab" @click="enter" />
+
+    <template v-if="isDev">
+      <div class="file-button" @click="dialogVisible = true">获取excel</div>
+      <el-dialog v-model="dialogVisible" title="读取文件" align-center width="90%">
+        <ReadExcel class="excel" @file-upload="getFileData" />
+        <template #footer>
+          <el-button @click="onConfirm">确定</el-button>
+        </template>
+      </el-dialog>
+    </template>
   </div>
 </template>
 <script setup lang="ts">
 import { useRouter } from "vue-router"
 import { useExperimentStore } from "@/stores/experimentStore"
 import ReadExcel from "@/components/ReadExcel.vue"
-import { ref, watchEffect } from "vue"
-interface Route {
-  name: string
-  route: string
-  EnglishName:string
-}
+
 const store = useExperimentStore()
 const router = useRouter()
-const routes = [
-  {
-    name: "人类免疫缺陷病毒（HIV）的筛查",
-    EnglishName: "Screening for Human Immunodeficiency Virus (HIV)",
-    route: "/experiment-page",
-  },
-]
-const goTo = (item: Route) => {
-  store.name = item.name
-  store.EnglishName = item.EnglishName
-  router.push(item.route)
+
+const enter = () => {
+  router.push("/experiment-page")
 }
 
 const dialogVisible = ref(false)
@@ -51,33 +39,28 @@ const onConfirm = () => {
   dialogVisible.value = false
   // store.saveExperimentInfo(fileData.value)
 }
+
+const isDev = computed(() => {
+  return import.meta.env.DEV
+})
 </script>
 <style scoped lang="scss">
 .homepage {
+  position: relative;
   width: 100%;
   height: 100vh;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 2rem;
-  background: no-repeat center url("@/assets/img/experiment_bg_black.png");
+  background: no-repeat center url("@/assets/img/homepage/bg.png");
   background-size: 100% 100%;
 
   .tab {
-    width: 25rem;
-    height: 5rem;
+    position: absolute;
+    left: 14%;
+    bottom: 20%;
+    width: 20vw;
+    height: 11vh;
     line-height: 5rem;
     text-align: center;
-    background: no-repeat center url("@/assets/img/little-tab.png");
-    background-size: 100% 100%;
-    font-size: 1.5rem;
     cursor: pointer;
-
-    &:hover {
-      background-image: url("@/assets/img/little_tab_active.png") !important;
-    }
   }
 
   .file-button {
