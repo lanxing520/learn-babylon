63ec78d92eacbd7f51a23abecbf507f59d4a67d1
lanxing520 <1461626683@qq.com>
2025-06-10 09:23:41 +0800

    修改实验步骤信息样式

--- src/views/scene/experiment/simulation/methods/components/ExperimentMessage.vue
+++ src/views/scene/experiment/simulation/methods/components/ExperimentMessage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="now-step">当前步骤:{{ active?.name }}</div>
-  <div class="left-button-wrapper">
+  <!-- <div class="now-step">{{ active?.name }}</div> -->
+  <div class="left-button-wrapper" :class="{ 'hide-left': isHideLeft }">
     <div ref="scrollRef" class="animation-list hide-scrollbar">
       <div
         class="animation-item text-truncate"
@@ -13,13 +13,18 @@
         {{ item.name }}
       </div>
     </div>
+    <div class="hide-button-left" :class="{ change: isHideLeft }" @click="hideLeft"></div>
   </div>
-  <div class="center-bottom-desc">
-    <div class="tips-container" v-show="expInfo.tipMessage">{{ expInfo.tipMessage }}</div>
-    <div v-show="warmTips" class="warm-tips-container">
-      {{ warmTips }}
-    </div>
-    <span>{{ active?.desc }} </span>
+  <div class="center-bottom-desc" :class="{ 'hide-down': isHideDown }">
+    <template v-if="!isHideDown">
+      <div class="tips-container" v-show="expInfo.tipMessage">{{ expInfo.tipMessage }}</div>
+      <div v-show="warmTips" class="warm-tips-container">
+        {{ warmTips }}
+      </div>
+      <span>{{ active?.desc }}</span>
+      <img src="@/assets/img/experiment/提示-icon.png" class="right-top-icon" />
+    </template>
+    <div class="hide-button-bottom" :class="{ change: isHideDown }" @click="hideDown"></div>
   </div>
 
   <el-button class="end-button" v-if="isFinished" @click="endExperiment">结束并提交</el-button>
@@ -39,7 +44,6 @@ import { useExperimentStore, experimentScore } from "@/stores/experimentStore"
 import ExpQuestion from "./ExpQuestion.vue"
 import { isFinished, warmTips } from "../common/stepManager"
 import { upload } from "@/api/rainier.ts"
-import http from "@/api/request"
 
 interface Step {
   index: null | number
@@ -146,6 +150,16 @@ function scrollToActiveItem(index: number) {
     })
   }
 }
+
+const isHideLeft = ref(false)
+const hideLeft = () => {
+  isHideLeft.value = !isHideLeft.value
+}
+const isHideDown = ref(false)
+const hideDown = () => {
+  isHideDown.value = !isHideDown.value
+}
+
 onMounted(() => {})
 </script>
 
@@ -169,10 +183,10 @@ onMounted(() => {})
 }
 .now-step {
   position: absolute;
-  left: 1rem;
+  right: 1rem;
   top: 4rem;
   color: #fff;
-  background: no-repeat center url("@/assets/img/experiment/当前步骤.png");
+  background: no-repeat center url("@/assets/img/experiment/步骤.png");
   background-size: 100% 100%;
   width: 10rem;
   height: 3rem;
@@ -188,33 +202,50 @@ onMounted(() => {})
   left: 0;
   top: 50%;
   transform: translateY(-50%);
-  width: 12rem;
+  width: 17rem;
   height: 55%;
+  transition: width 0.5s ease-in-out;
 
   .animation-list {
+    width: 100%;
     height: 100%;
     overflow: hidden auto;
 
     .animation-item {
-      width: 12rem;
-      height: 3rem;
-      line-height: 3rem;
+      width: 17rem;
+      height: 4rem;
+      line-height: 4rem;
       text-align: center;
       background: no-repeat center url("@/assets/img/experiment/步骤.png");
       background-size: 100% 100%;
-      color: #75d2fa;
+      color: #fff;
       cursor: pointer;
       border-radius: 15px;
-      margin-bottom: 0.55rem;
-      &:hover {
-        background-color: rgba(0, 255, 255, 0.3);
-      }
-
+      margin-bottom: 1rem;
+      letter-spacing: 2px;
+      font-size: 1.5rem;
       &.finish {
         background-image: url("@/assets/img/experiment/步骤finish.png");
       }
     }
   }
+}
+.hide-button-left {
+  position: absolute;
+  width: 3rem;
+  height: 3rem;
+  right: -3rem;
+  top: 50%;
+  background: no-repeat center url("@/assets/img/experiment/arrow_left.png");
+  background-size: 100% 100%;
+  cursor: pointer;
+
+  &.change {
+    transform: rotate(180deg);
+  }
+}
+.hide-left {
+  width: 0 !important;
 }
 
 .center-bottom-desc {
@@ -226,9 +257,11 @@ onMounted(() => {})
   transform: translateX(-50%);
   background: no-repeat center url("@/assets/img/experiment/文本框.png");
   background-size: 100% 100%;
-  padding: 1rem;
+  padding: 2rem 1rem;
   color: #fff;
-
+  transition:
+    min-height,
+    0.5s ease-in-out;
   .tips-container {
     position: absolute;
     bottom: calc(100% + 1rem);
@@ -248,5 +281,34 @@ onMounted(() => {})
     background: no-repeat center url("@/assets/img/experiment/温馨提醒.png");
     z-index: 99;
   }
+
+  .right-top-icon {
+    display: block;
+    position: absolute;
+    right: 0.1rem;
+    top: 0.1rem;
+    scale: 0.8;
+  }
+}
+
+.hide-button-bottom {
+  position: absolute;
+  width: 3rem;
+  height: 3rem;
+  top: -3rem;
+  left: 50%;
+  background: no-repeat center url("@/assets/img/experiment/arrow_down.png");
+  background-size: 100% 100%;
+  cursor: pointer;
+
+  &.change {
+    transform: rotate(180deg);
+  }
+}
+.hide-down {
+  // display: none;
+  padding: 0;
+  height: 0;
+  min-height: 0;
 }
 </style>
